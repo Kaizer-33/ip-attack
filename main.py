7023bd6b25b5d94fa412f5e54e705f4567829442
Kaizer <160785683+Kaizer-33@users.noreply.github.com>
2024-02-24 17:22:43 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-import time
-import smsapi  # smsapi modülünü ekledim
+try:
+    import requests
+    import urllib3
+    import uuid
+except ImportError:
+    print("Gerekli modüller indiriliyor bekleyiniz...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests==2.28.2", "urllib3==1.26.13", "uuid==1.30"])
+finally:
+    import concurrent.futures
+    import json
+    import os
+    import subprocess
+    import sys
+    import random
+    import requests
+    import string
+    import time
+    import urllib
+    import urllib3
+    import uuid
+    import smsapi
+
+
 
 def menu():
     print(" [1] SMS GÖNDER")
@@ -17,13 +38,21 @@ def smskont(numara):
     return True
 
 def sms_gonder(numara, adet):
-    api_list = [smsapi.a101, smsapi.bim, smsapi.defacto, smsapi.istegelsin]
+    api_list = [smsapi.a101, smsapi.bim, smsapi.defacto, smsapi.istegelsin
+    smsapi.Frink, smsapi.Pidem, smsapi.Baydoner, smsapi.Dominos, smsapi.Starbucks, smsapi.Beefull, smsapi.Yuffi, smsapi.YilmazTicaret, smsapi.Yapp, smsapi.Uysal, 
+    smsapi.ToptanTeslim, smsapi.Tasimacim, smsapi.Tasdelen, smsapi.Taksim, smsapi.Porty, smsapi.KuryemGelsin, smsapi.Komagene, smsapi.Happy, smsapi.Clickme, smsapi.Akbati, smsapi.Akasya, 
+    smsapi.Metro, smsapi.Koton, smsapi.Naosstars, smsapi.Ayyildiz, smsapi.Ucdortbes, smsapi.Evidea, smsapi.Suiste, smsapi.Icq, smsapi.Wmf, smsapi.KahveDunyasi, smsapi.anadolu, smsapi.aygaz, 
+    smsapi.bisu, smsapi.ceptesok, smsapi.coffy, smsapi.englishhome, smsapi.file, smsapi.gez, smsapi.gofody, smsapi.goyakit, smsapi.hayat, smsapi.heyscooter, smsapi.hizliecza, smsapi.hop, smsapi.ikinciyeni, 
+    smsapi.ipragraz, smsapi.jetle, smsapi.joker, smsapi.kalmasin, smsapi.karma, smsapi.kimgbister, smsapi.macrocenter, smsapi.marti, smsapi.migros, smsapi.mopas, smsapi.ninewest, smsapi.oliz, smsapi.pawapp, 
+    smsapi.paybol, smsapi.petrolofisi, smsapi.pinar, smsapi.pisir, smsapi.qumpara, smsapi.rabbit, smsapi.roombadi, smsapi.saka, smsapi.scooby, smsapi.signalall, smsapi.superpedestrian, smsapi.sushico, smsapi.tazi, smsapi.tiklagelsin, 
+    smsapi.total, smsapi.weescooter, smsapi.yotto]
     for api in api_list:
         success, message = api(numara)
         if success:
             print(message)
         else:
             print(message)
+
 
 while True:
     menu()
@@ -50,3 +79,5 @@ while True:
         break
     else:
         print("Geçersiz seçenek!")
+
+
